9ce216fb43a421eee87a224c01105cc7bbf5012c
Samatar Axmed <Samatar_267@hotmail.com>
2017-05-29 14:07:23 +0100

    Created first reducer BookReducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Harry Potter' },
+    { title: 'JavaScript: The Good Parts' },
+    { title: 'Eloquent Ruby' },
+    { title: 'The Dark Tower' },
+  ];
+}
